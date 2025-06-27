030240657bf363a4fc015557df3ef605be3d6d77
Kripa Jethwani <kripa@atliq.com>
2025-06-27 11:49:07 +0530

    Fixed: Import errors after code was commented out

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -3,9 +3,9 @@ import Header from '@/components/Header';
 import HeroSection from '@/components/HeroSection';
 import AboutSection from '@/components/AboutSection';
 import AIChallengesSection from '@/components/AIChallengesSection';
-import ScheduleSection from '@/components/ScheduleSection';
+// import ScheduleSection from '@/components/ScheduleSection';
 import TimelineSection from '@/components/TimelineSection';
-import ProblemsSection from '@/components/ProblemsSection';
+// import ProblemsSection from '@/components/ProblemsSection';
 import EnhancedRegistrationSection from '@/components/EnhancedRegistrationSection';
 import Footer from '@/components/Footer';
 
@@ -17,8 +17,8 @@ const Index = () => {
       <AboutSection />
       <AIChallengesSection />
       <TimelineSection />
-      <ScheduleSection />
-      <ProblemsSection />
+      {/* <ScheduleSection /> */}
+      {/* <ProblemsSection /> */}
       <EnhancedRegistrationSection />
       <Footer />
     </div>
